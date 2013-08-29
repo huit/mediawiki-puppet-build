3c63199cd25183cc7706153c3a602dae105d7034
Steve Huff <shuff@vecna.org>
2013-08-29 12:13:00 -0400

    Capturing cheesy env2yaml.rb

---
+++ files/env2yaml.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'yaml'
+
+# parse args to determine env var prefix
+default_prefix = 'NEPHO_'
+prefix = default_prefix
+ARGV.each_with_index {|arg, i| arg =~ /^(-p|--prefix)$/ && prefix = ARGV[i + 1]}
+prefix || prefix = default_prefix
+
+# extract env vars matching prefix and put them in a hash
+extracted = {}
+ENV.select {|key, value| key =~ /^#{prefix}/}.each {|pair| extracted[pair[0]] = pair[1]}
+
+# emit the hash
+puts extracted.to_yaml
