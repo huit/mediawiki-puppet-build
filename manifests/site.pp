c61263753e586a91577d4762812252bbb3f1cddc
Steve Huff <shuff@vecna.org>
2013-09-11 18:10:27 -0400

    Add support for $nepho_mail_relay_host
    This will be set to the SES endpoint

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,6 +21,7 @@ node default {
   $nepho_s3_bucket         = hiera('NEPHO_S3_BUCKET',false)
   $nepho_s3_access_key     = hiera('NEPHO_S3_BUCKET_ACCESS','no_s3_bucket_access_provided')
   $nepho_s3_secret_key     = hiera('NEPHO_S3_BUCKET_KEY','no_s3_bucket_secret_provided')
+  $nepho_mail_relay_host   = hiera('NEPHO_MAIL_RELAY_HOST','')
 
   $probe_interval     = "30s"
   $probe_timeout      = "10s"
@@ -34,8 +35,13 @@ node default {
     logoutput => 'on_failure',
   }
 
-  class { 'postfix':
-    smtp_relay => true,
+  if $nepho_mail_relay_host {
+    class { 'postfix':
+      relay_host => $nepho_mail_relay_host,
+    }
+  }
+  else {
+    class { 'postfix': }
   }
 
   if $nepho_instance_role {
